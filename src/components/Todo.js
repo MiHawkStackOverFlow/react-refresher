c1da6984679de5bfbca222d929e2fc4800aa53f2
Abhishek Sharma <abhivsharma8@gmail.com>
2023-11-30 18:52:03 +0530

    separated todo component

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Todo() {
+  return (
+    <div className="card">
+      <h2>TITLE</h2>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
