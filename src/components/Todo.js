1e9586461c717495aabcb56b6d4503a82d868959
Abhishek Sharma <abhivsharma8@gmail.com>
2023-11-30 19:09:22 +0530

    event handler on a button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 export default function Todo(props) {
+  function deleteHandler() {
+    console.log('clicked ', props.text);
+  }
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn"  onClick={deleteHandler}>Delete</button>
       </div>
     </div>
   );
