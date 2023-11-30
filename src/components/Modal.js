cea26350e3327482c5bd8a138d11e19ba97523b4
Abhishek Sharma <abhivsharma8@gmail.com>
2023-11-30 20:10:27 +0530

    event props 2 ways Modal and Backdrop components

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-export default function Modal() {
+export default function Modal(props) {
+  function cancelHandler() {
+    props.onCancel();
+  }
+  function confirmModalHandler() {
+    props.onConfirm();
+  }
   return (
     <div className="modal">
       <p>Are you sure?</p>
-      <button className="btn btn--alt">Cancel</button>
-      <button className="btn">Confirm</button>
+      <button className="btn btn--alt" onClick={cancelHandler}>
+        Cancel
+      </button>
+      <button className="btn" onClick={confirmModalHandler}>
+        Confirm
+      </button>
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,14 +7,17 @@ export default function Todo(props) {
   function deleteHandler() {
     setModalIsOpen(true);
   }
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn"  onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
-      { modalIsOpen && <Modal /> }
-      { modalIsOpen && <Backdrop /> }
+      { modalIsOpen && <Modal onCancel={closeModalHandler}  onConfirm={closeModalHandler} /> }
+      { modalIsOpen && <Backdrop onCancel={closeModalHandler} /> }
     </div>
   );
 }
