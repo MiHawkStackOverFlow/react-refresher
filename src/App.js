d980f410c9c9d4bdddb8b84e1749c3dd6a39e5ce
Abhishek Sharma <abhivsharma8@gmail.com>
2023-12-01 12:03:16 +0530

    added new pages components and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
+
 function App() {
   return (
     <div>
-     
+      <Switch>
+        <Route exact path="/">
+          <AllMeetupsPage />
+        </Route>
+        <Route exact path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route exact path="/favorites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewMeetupPage() {
+  return (
+    <div>NewMeetup</div>
+  )
+}
