e9e2bffd2939f89da3c1e2f1fb50fdb6cbd86bab
Abhishek Sharma <abhivsharma8@gmail.com>
2023-12-01 18:55:35 +0530

    POST call to firebase server to add meetup data

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import React from "react";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 export default function NewMeetupPage() {
-  function addMeetupHandler(meetupData) {}
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-refresher-69d02-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
       <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
-  )
+  );
 }
